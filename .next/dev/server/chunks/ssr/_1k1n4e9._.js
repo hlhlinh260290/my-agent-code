89defd36370add22e4ae0fbfa8343bbba2e60acf
linh ha <hlhlinh@gmail.com>
2026-09-07 13:02:04 +0700

    test 2

--- .next/dev/server/chunks/ssr/_1k1n4e9._.js
+++ .next/dev/server/chunks/ssr/_1k1n4e9._.js
@@ -13,7 +13,7 @@ var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f2e$pnpm$2f$ne
 ;
 function Dashboard() {
     // Lỗi ESLint: 'unusedData' is assigned a value but never used.
-    // const unusedData = "Hello";
+    const unusedData = "Hello";
     (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f2e$pnpm$2f$next$40$16$2e$3$2e$4_$40$babel$2b$core$40$7$2e$29$2e$7_$40$types$2b$node$40$22$2e$20$2e$1_react$2d$dom$40$19$2e$2$2e$8_react$40$19$2e$2$2e$8_$5f$react$40$19$2e$2$2e$8$2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["useEffect"])(()=>{
         console.log("Dashboard loaded");
     }, []); // Cố tình bỏ quên dependency nếu có gọi API
